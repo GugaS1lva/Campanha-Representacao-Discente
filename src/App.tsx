2cde3567523c29dfe9f38052f3d2203f0a53967f
Gustavo Silva <contatogustavosilva017@outlook.com>
2024-02-29 10:20:26 -0300

    Finished Project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import img2 from './assets/2.png'
+import img3 from './assets/3.png'
+import img4 from './assets/4.png'
+import img5 from './assets/5.png'
+
+function App() {
+  return (
+    <main className="w-full h-fulw-full bg-zinc-900 flex flex-col items-center mx-auto">
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
+      <img src={img4} alt="" />
+      <img src={img5} alt="" />
+    </main>
+  )
+}
+
+export default App
